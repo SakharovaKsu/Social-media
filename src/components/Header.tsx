df78da0b192d16bb487183dac51f84ba1ed128f1
SakharovaKsu <nagis38@gmail.com>
2023-04-28 23:06:07 +0300

    Заливаю сборку и начальная стадия компонентов

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <a>
+                <img src='https://img.icons8.com/flat-round/512/fox--v1.png' />
+            </a>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://catherineasquithgallery.com/uploads/posts/2021-12/1639651365_253-catherineasquithgallery-com-p-fon-akvarel-rozovii-pastelnii-kremovii-419.jpg' />
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New posts
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
