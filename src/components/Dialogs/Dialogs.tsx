50e0a7d0f20a9d901e2caa3f34194099906fce13
SakharovaKsu <nagis38@gmail.com>
2023-05-04 13:12:45 +0300

    Временно убрала типизацию в диалоге, устранила баг

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,20 +2,21 @@ import React, {FC} from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
-type DialogsType = {
-    id: number
-    name: string
-}
+// type DialogsType = {
+//     id: string
+//     name: string
+// }
+//
+// type MessagesType = {
+//     message: string
+// }
+//
+// type DialogsMessagesType = {
+//     dialogs: DialogsType
+//     messages: MessagesType
+// }
 
-type MessagesType = {
-    message: string
-}
-
-type DialogsMessagesType = {
-    dialogs: DialogsType
-    messages: MessagesType
-}
-const DialogItem:FC<DialogsType> = (props) => {
+const DialogItem = (props: any) => {
     return (
         <li className={s.item}>
             <NavLink to={`dialogs/${props.id}`}>{props.name}</NavLink>
@@ -23,18 +24,19 @@ const DialogItem:FC<DialogsType> = (props) => {
     )
 }
 
-const Message: FC<MessagesType> = (props) => {
+const Message = (props: any) => {
     return <div className={s.message}>{props.message}</div>
 };
-const Dialogs:FC<DialogsMessagesType> = () => {
+
+const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <ul className={s.list}>
-                <DialogItem name='Dima' id={1} />
-                <DialogItem name='Valera' id={2} />
-                <DialogItem name='Sveta' id={3} />
-                <DialogItem name='Olga' id={4} />
-                <DialogItem name='Paul' id={5} />
+                <DialogItem name='Dima' id='1' />
+                <DialogItem name='Valera' id='2' />
+                <DialogItem name='Sveta' id='3' />
+                <DialogItem name='Olga' id='4' />
+                <DialogItem name='Paul' id='5' />
             </ul>
             <div className={s.messages}>
                 <Message message='Hi'/>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,16 +7,16 @@ const Navbar = () => {
         <nav className={s.nav}>
             <ul className={s.list}>
                 <li className={s.item}>
-                    <NavLink to='profile' activeClassName={s.active} >Profile</NavLink>
+                    <NavLink to='/profile' activeClassName={s.active} >Profile</NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='dialogs' activeClassName={s.active} >Messages</NavLink>
+                    <NavLink to='/dialogs' activeClassName={s.active} >Messages</NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='news' activeClassName={s.active} >News</NavLink>
+                    <NavLink to='/news' activeClassName={s.active} >News</NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='music' activeClassName={s.active} >Music</NavLink>
+                    <NavLink to='/music' activeClassName={s.active} >Music</NavLink>
                 </li>
             </ul>
         </nav>
