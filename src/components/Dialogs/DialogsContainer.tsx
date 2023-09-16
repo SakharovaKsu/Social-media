aa274df065dd0b08f8f42c8f0289f67206785d78
SakharovaKsu <nagis38@gmail.com>
2023-09-16 18:34:29 +0300

    Удаление лишнего

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,34 +1,9 @@
 import React from 'react';
 import {addMassageAC, updateNewMessageTextAC} from '../../redux/dialogsReducer';
 import {StoreType} from '../../redux/reduxStore';
-import Dialogs from './Dialogs';
 import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
-
-// type DialogsType = {
-//     store: StoreType
-// }
-// const DialogsContainer:FC<DialogsType> = ({store}) => {
-//
-//     const dialogsPage = store.getState().dialogsPage
-//     const dispatch = store.dispatch.bind(store)
-//
-//     const addMessages = () => {
-//         dispatch(addMassageAC())
-//     }
-//
-//     const updateNewMessageText = (text: string) => {
-//         dispatch(updateNewMessageTextAC(text))
-//     }
-//
-//     return (
-//         <Dialogs
-//             dialogsPage={dialogsPage}
-//             addMessagesCallback={addMessages}
-//             updateNewMessageTextCallback={updateNewMessageText}
-//         />
-//     )
-// }
+import Dialogs from './Dialogs';
 
 // Контейнерную компоненту законектили с компонентой Dialogs
 // Первая функция в connect создает контейнерную компоненту, внутри рендерит презентационную компоненту (Dialogs), и внутрь качестве пропсов передает из функций те свойства, которые ретурнятся в качестве объектов, на выхоже из 2-ой функции получим - <Dialogs dialogsPage={dialogsPage} addMessagesCallback={addMessages} updateNewMessageTextCallback={updateNewMessageText} />
